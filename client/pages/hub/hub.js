cbb303094faebd67781eaaf59ba67ba56918392c
Christopher Bennage <christopher@bennage.com>
2012-07-10 15:04:39 -0700

    initial commit of WinJS client; just a shell for serving up samples

---
+++ client/pages/hub/hub.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    var navigate = WinJS.Navigation.navigate,
+        animation = WinJS.UI.Animation;
+
+    var menu = [
+    {
+        name: 'Image Crop',
+        page: 'crop'
+    },
+    {
+        name: 'Stocks',
+        page: 'stocks'
+    }];
+
+
+    function gotoPage(args) {
+        args.detail.itemPromise.then(function (item) {
+            var id = item.data.page;
+            navigate('/pages/' + id + '/' + id + '.html'); 
+        });
+    }
+
+    WinJS.UI.Pages.define('/pages/hub/hub.html', {
+
+        ready: function (element, options) {
+            var listView = document.querySelector('[data-win-control="WinJS.UI.ListView"]').winControl;
+            listView.itemDataSource = new WinJS.Binding.List(menu).dataSource;
+            listView.addEventListener('iteminvoked', gotoPage);
+
+            animation.enterPage(document.querySelectorAll('header,section'));
+        },
+
+        updateLayout: function (element, viewState, lastViewState) {
+            /// <param name='element' domElement='true' />
+            /// <param name='viewState' value='Windows.UI.ViewManagement.ApplicationViewState' />
+            /// <param name='lastViewState' value='Windows.UI.ViewManagement.ApplicationViewState' />
+
+            // TODO: Respond to changes in viewState.
+        },
+
+        unload: function () {
+            // TODO: Respond to navigations away from this page.
+        }
+    });
+})();
