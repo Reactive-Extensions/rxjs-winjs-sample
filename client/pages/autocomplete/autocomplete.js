31c9db185d64ac1a050390edd2d0709ed2377917
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2012-07-16 20:19:59 -0400

    Updating autocomplete bindings

--- client/pages/autocomplete/autocomplete.js
+++ client/pages/autocomplete/autocomplete.js
@@ -12,7 +12,7 @@
 
     function searchWikipedia(term) {
         var url = 'http://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='
-            + window.encodeURI(term) + '&callback=JSONPCallback';
+            + window.encodeURI(term);// + '&callback=JSONPCallback';
         return WinJS.xhr({url: url}).toObservable();
     }
 
@@ -24,11 +24,11 @@
 
         // Get input/output
         var input = document.querySelector('#rxInput');
-        var results = document.querySelector('#rxResults');
+        var resultsList = document.querySelector('#rxResults');
 
         var keyup = Rx.Observable.fromEvent(input, 'keyup')
             .select(function (ev) {
-                return ev.currentTarget;
+                return ev.currentTarget.value;
             })
             .where(function (text) {
                 return text.length > 2;
@@ -41,6 +41,9 @@
                 return searchWikipedia(text);
             })
             .switchLatest()
+            .select(function (response) {
+                return JSON.parse(response.response);
+            })
             .where(function (results) {
                 return results.length === 2;
             });
@@ -48,23 +51,23 @@
         subscription = searcher.subscribe(
             function (data) {
                 var results = data[1];
-                clearChildren(results);
+                clearChildren(resultsList);
 
                 results.forEach(function (result) {
                     var li = document.createElement('li');
                     li.innerText = result;
-                    results.appendChild(li);
+                    resultsList.appendChild(li);
                 });
             },
             function (err) {
-                clearChildren(results);
+                clearChildren(resultsList);
                 var li = document.createElement('li');
                 li.innerText = err;
-                results.appendChild(li);
+                resultsList.appendChild(li);
             });
     }
 
-    WinJS.UI.Pages.define("/pages/autocomplete.html", {
+    WinJS.UI.Pages.define("/pages/autocomplete/autocomplete.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
