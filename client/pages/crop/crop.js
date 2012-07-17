10e0b79d1ae20f9f80013022456d42e9edf63e82
Christopher Bennage <christopher@bennage.com>
2012-07-16 17:37:24 -0700

    implemented crop with RxJS

--- client/pages/crop/crop.js
+++ client/pages/crop/crop.js
@@ -1,168 +1,206 @@
-﻿(function () {
+﻿/// <reference path="../../rxjs/rx.js" />
+/// <reference path="../../rxjs/reactivewinjs.js" />
+
+(function () {
     'use strict';
 
-    var utilities = WinJS.Utilities;
+    var utilities = WinJS.Utilities,
+        promise = WinJS.Promise;
+
+    // Start with `ready`. It's the function that is invoked when the page is loaded.
 
     WinJS.UI.Pages.define('/pages/crop/crop.html', {
 
-        ready: function (element, options) {
+        // a model for the region of the image about to be cropped
+        boundingBox: {
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 0
+        },
+        // an array of draggable elements that modify the crop region
+        handles: [],
+        loadImage: function () {
+            var overlay = this.overlay;
 
-            var tools = [];
-            var rect = {
-                x: null,
-                y: null,
-                x2: null,
-                y2: null
-            };
+            // This promise completes when the img is loaded.
+            return new promise(function (complete, error, progress) {
 
-            var selectedElement = null;
+                // `buffer` displays the actual image to crop
+                var buffer = document.querySelector('#buffer');
 
+                var img = document.createElement('img');
+                img.src = '/pages/crop/images/leaf twirl.jpg';
+
+                img.addEventListener('load', function (args) {
+                    overlay.width = img.width;
+                    overlay.height = img.height;
+
+                    buffer.width = img.width;
+                    buffer.height = img.height;
+                    buffer.getContext('2d').drawImage(img, 0, 0);
+
+                    // return the dimensions of the image we just loaded
+                    complete({
+                        width: img.width,
+                        height: img.height
+                    });
+
+                }, false);
+            });
+        },
+        initBoundingBox: function (size) {
+            this.boundingBox.x = 0;
+            this.boundingBox.y = 0;
+            this.boundingBox.x2 = size.width;
+            this.boundingBox.y2 = size.height;
+            return promise.wrap(this.boundingBox);
+        },
+        createHandles: function (boundingBox) {
             var container = document.querySelector('#container'),
-                buffer = document.querySelector('#buffer'),
-                overlay = document.querySelector('#overlay'),
-                // We use a DOM element to draw a dotted line, because
-                // it is a bit complicated to do so with a canvas.
-                box = document.querySelector('#bounding-box');
+                handles = [];
 
-            var ctx = overlay.getContext('2d');
-
-            var img = document.createElement('img');
-            img.src = '/pages/crop/images/leaf twirl.jpg';
-            img.addEventListener('load', function (args) {
-                overlay.width = img.width;
-                overlay.height = img.height;
-
-                buffer.width = img.width;
-                buffer.height = img.height;
-                buffer.getContext('2d').drawImage(img, 0, 0);
-
-                resetHandles();
-            }, false);
-
-            function resetHandles() {
-                rect.x = 0;
-                rect.y = 0;
-                rect.x2 = overlay.width;
-                rect.y2 = overlay.height;
-
-                var w = rect.x2 - rect.x;
-                var h = rect.y2 - rect.y;
-                drawOverlay(rect.x, rect.y, w, h);
-            }
-
-            function drawOverlay(x, y, w, h) {
-                var dotted_line = box.style;
-
-                ctx.globalCompositeOperation = 'source-over';
-
-                ctx.clearRect(0, 0, overlay.width, overlay.height);
-
-                ctx.fillStyle = 'rgba(0,0,0,0.7)';
-                ctx.fillRect(0, 0, overlay.width, overlay.height);
-
-                ctx.globalCompositeOperation = 'destination-out';
-                ctx.fillStyle = 'rgba(0,0,0,1)';
-                ctx.fillRect(x, y, w, h);
-                ctx.fill();
-
-                tools.forEach(function (tool) { tool['position'](); });
-
-                dotted_line.top = y + 'px';
-                dotted_line.left = x + 'px';
-                dotted_line.width = w + 'px';
-                dotted_line.height = h + 'px';
-            }
-
-            function animateOverlay() {
-                var w = rect.x2 - rect.x;
-                var h = rect.y2 - rect.y;
-
-                drawOverlay(rect.x, rect.y, w, h);
-
-                if (selectedElement) {
-                    window.requestAnimationFrame(animateOverlay);
-                }
-            }
-
-            createHandle(container, 'tl', function () {
-                this.style.top = rect.y + 'px';
-                this.style.left = rect.x + 'px';
-            });
-
-            createHandle(container, 'tr', function () {
-                this.style.top = rect.y + 'px';
-                this.style.left = rect.x2 + 'px';
-            });
-
-            createHandle(container, 'bl', function (s) {
-                this.style.top = rect.y2 + 'px';
-                this.style.left = rect.x + 'px';
-            });
-
-            createHandle(container, 'br', function (s) {
-                this.style.top = rect.y2 + 'px';
-                this.style.left = rect.x2 + 'px';
-            });
-
-            function createHandle(container, id, position) {
+            function createHandle(id, render, updateModel) {
                 var handle = document.createElement('div');
                 utilities.addClass(handle, 'handle');
                 handle.setAttribute('id', id);
                 container.appendChild(handle);
 
-                handle.addEventListener('mousedown', function (args) {
-                    selectedElement = args.currentTarget;;
-                    window.requestAnimationFrame(animateOverlay);
-                }, false);
+                // `render` allows us to visually update the handle after it has been dragged
+                handle['render'] = render;
+                // `updateModel` allows us to modify the correct part of the crop region model
+                handle['updateModel'] = updateModel;
 
-                overlay.addEventListener('mousemove', function (args) {
-                    if (!selectedElement) return;
-
-                    var t = selectedElement,
-                        x = (args.offsetX),
-                        y = (args.offsetY);
-
-                    switch (t.id) {
-                        case 'tl':
-                            rect.x = x;
-                            rect.y = y;
-                            break;
-                        case 'tr':
-                            rect.y = y;
-                            rect.x2 = x;
-                            break;
-                        case 'bl':
-                            rect.x = x;
-                            rect.y2 = y;
-                            break;
-                        case 'br':
-                            rect.y2 = y;
-                            rect.x2 = x;
-                            break;
-                    }
-
-                }, true);
-
-                document.addEventListener('mouseup', function (args) {
-                    selectedElement = null;
-                }, true);
-
-                handle['position'] = position;
-
-                tools.push(handle);
+                handles.push(handle);
             };
+
+            // top left
+            createHandle('tl', function () {
+                this.style.top = boundingBox.y + 'px';
+                this.style.left = boundingBox.x + 'px';
+            }, function (x, y) {
+                boundingBox.x = x;
+                boundingBox.y = y;
+            });
+
+            //top right
+            createHandle('tr', function () {
+                this.style.top = boundingBox.y + 'px';
+                this.style.left = boundingBox.x2 + 'px';
+            }, function (x, y) {
+                boundingBox.y = y;
+                boundingBox.x2 = x;
+            });
+
+            // bottom left
+            createHandle('bl', function (s) {
+                this.style.top = boundingBox.y2 + 'px';
+                this.style.left = boundingBox.x + 'px';
+            }, function (x, y) {
+                boundingBox.x = x;
+                boundingBox.y2 = y;
+            });
+
+            // bottom right
+            createHandle('br', function (s) {
+                this.style.top = boundingBox.y2 + 'px';
+                this.style.left = boundingBox.x2 + 'px';
+            }, function (x, y) {
+                boundingBox.y2 = y;
+                boundingBox.x2 = x;
+            });
+
+            // render the handles in their initial positiions
+            handles.forEach(function (element) { element['render'](); });
+
+            return new promise(function (complete, error, progress) {
+                complete(handles);
+            });
         },
+        drawOverlay: function (x, y, w, h) {
+            var boundingBox = this.boundingBox,
+                x = boundingBox.x,
+                y = boundingBox.y,
+                w = boundingBox.x2 - boundingBox.x,
+                h = boundingBox.y2 - boundingBox.y,
+                ctx = this.ctx;
 
-        updateLayout: function (element, viewState, lastViewState) {
-            /// <param name='element' domElement='true' />
-            /// <param name='viewState' value='Windows.UI.ViewManagement.ApplicationViewState' />
-            /// <param name='lastViewState' value='Windows.UI.ViewManagement.ApplicationViewState' />
+            ctx.globalCompositeOperation = 'source-over';
 
-            // TODO: Respond to changes in viewState.
+            ctx.clearRect(0, 0, overlay.width, overlay.height);
+
+            ctx.fillStyle = 'rgba(0,0,0,0.7)';
+            ctx.fillRect(0, 0, overlay.width, overlay.height);
+
+            ctx.globalCompositeOperation = 'destination-out';
+            ctx.fillStyle = 'rgba(0,0,0,1)';
+            ctx.fillRect(x, y, w, h);
+            ctx.fill();
+
+            if (this.handles)
+                this.handles.forEach(function (tool) { tool['render'](); });
         },
+        respondToGestures: function (handles) {
+            var self = this,
+                fromEvent = Rx.Observable.fromEvent,
+                boundingBox = this.boundingBox,
+                moves = fromEvent(self.overlay, 'mousemove');
 
-        unload: function () {
-            // TODO: Respond to navigations away from this page.
+            self.handles = handles;
+
+            fromEvent(handles, 'mousedown')
+                // When the mouse is down on a handle, return the handle element
+                .select(function (x) { return x.currentTarget; })
+                .merge(fromEvent(document, 'mouseup')
+                // However, when the mouse is up (anywhere on the document),
+                // then don't return an element 
+                .select(function (x) { return false; }))
+                // Then combine the handle element with the position of the 
+                // the mouse with respect to the overlay
+                .select(function (el) {
+                    return moves.select(function (args) {
+                        return {
+                            element: el,
+                            offsetX: args.offsetX,
+                            offsetY: args.offsetY
+                        };
+                    });
+                })
+                .switchLatest()
+                // Throw out any items that didn't have an element
+                .where(function (x) {
+                    return x.element !== false;
+                })
+                // Finally, update the bounding box model and request a redraw
+                .subscribe(function (data) {
+                    data.element.updateModel(data.offsetX, data.offsetY);
+                    window.requestAnimationFrame(self.drawOverlay.bind(self));
+                });
+        },
+        ready: function (element, options) {
+            var self = this;
+
+            var
+            // When we invoke these functions from a Promise
+            // they lose their binding to `this`.
+            // TODO: Is there a way to fix this?
+            initBoundingBox = self.initBoundingBox.bind(self),
+            createHandles = self.createHandles.bind(self),
+            respondToGestures = self.respondToGestures.bind(self);
+
+            // `overlay` allows us to darken the portion 
+            // of the image that will be removed.
+            this.overlay = document.querySelector('#overlay');
+            this.ctx = overlay.getContext('2d');
+
+            // We use a DOM element to draw a dotted line, because
+            // it is a bit complicated to do so with a canvas.
+            this.box = document.querySelector('#bounding-box');
+
+            this.loadImage()
+                .then(initBoundingBox)
+                .then(createHandles)
+                .then(respondToGestures);
         }
     });
 })();
